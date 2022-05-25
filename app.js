4bc6ad2baadbdf1d91abe95918b90b6cfd193672
leeyeonji28 <yeon102800@gmail.com>
2022-05-26 00:16:20 +0900

    할일리스트 API 구현

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "sbsst",
+  password: "sbs123414",
+  database: "todoapp_2022_05_25",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  dateStrings: true,
+});
 
 const app = express();
 const appPort = 3000;
@@ -12,15 +24,24 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get(`/todos`, (req, res) => {
-  return res.json([
-    {
-      id: 1,
-    },
-    {
-      id: 2,
-    },
-  ]);
+app.get(`/:user_code/todos`, async (req, res) => {
+  const { user_code } = req.params;
+
+  const [todoRows] = await pool.query(
+    `
+        SELECT *
+        FROM todo
+        WHERE user_code = ?
+        ORDER BY id DESC
+      `,
+    [user_code]
+  );
+
+  res.json({
+    resultCode: "S-1",
+    msg: "성공",
+    data: todoRows,
+  });
 });
 
 app.listen(appPort, () => {
